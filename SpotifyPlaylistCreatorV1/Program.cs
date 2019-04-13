15ab4e5202693ea9ca37def02bace96027117477
Ceri <Ceri@CERI-PC>
2019-04-13 10:41:18 +0100

    Add project files.

---
+++ SpotifyPlaylistCreatorV1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SpotifyPlaylistCreatorV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
